0e839cceedeb928dbf93c8fa6ddef34bd5012bbd
Rand3rino <nguyeran@mail.gvsu.edu>
2018-10-20 11:57:37 -0400

    ai move

--- src/com/atomicobject/othello/AI.java
+++ src/com/atomicobject/othello/AI.java
@@ -2,6 +2,9 @@ package com.atomicobject.othello;
 
 import java.util.Arrays;
 import java.util.ListIterator;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 public class AI {
 	
@@ -12,7 +15,25 @@ public class AI {
 	}
 
 	public int[] computeMove(GameState state) {
+		Gson gson = new Gson();
+		
+		int []move = new int[2];
+		
+		//LegalMoves(state);
+		
+		
+		int moveX = 4 ,moveY = 2;
+		move[0] = moveX;
+		move[1] = moveY;
+		
+//		JsonObject json = new JsonObject();
+//		json.add(gson.toJson(move), json );
+//		JsonArray jarray = new JsonArray();
+//		jarray.add(json);
+		
 		System.out.println("AI returning canned move for game state - " + state);
-		return moveList.next();
+		
+		//return jarray;
+		return move;
 	}
 }
