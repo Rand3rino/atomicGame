b8a369ba4d6bb125346dd9e25c1eff90bbd122e0
Rand3rino <nguyeran@mail.gvsu.edu>
2018-10-20 08:42:25 -0400

    load in premade files

--- src/atomicGame/AI.java
+++ src/atomicGame/AI.java
@@ -1,6 +1,18 @@
-package atomicGame;
+package com.atomicobject.othello;
+
+import java.util.Arrays;
+import java.util.ListIterator;
 
 public class AI {
-// here we go!
-	// comment 12
+	
+	ListIterator<int[]> moveList;
+
+	public AI(int[][] moves) {
+		moveList = Arrays.asList(moves).listIterator();
+	}
+
+	public int[] computeMove(GameState state) {
+		System.out.println("AI returning canned move for game state - " + state);
+		return moveList.next();
+	}
 }
