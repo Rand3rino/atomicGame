5673e60e752bb00b682326d53e8bea348cf6f1c7
Rand3rino <nguyeran@mail.gvsu.edu>
2018-10-20 13:25:30 -0400

    start to algs

--- src/com/atomicobject/othello/AI.java
+++ src/com/atomicobject/othello/AI.java
@@ -9,6 +9,7 @@ import com.google.gson.JsonObject;
 
 public class AI {
 
+	int player, opponent;
 	ListIterator<int[]> moveList;
 
 	public AI(int[][] moves) {
@@ -16,11 +17,14 @@ public class AI {
 	}
 
 	public int[] computeMove(GameState state) {
+		
+		player = state.getPlayer();
+		opponent = getOpponent();
+		
 		Gson gson = new Gson();
 
 		int[] move = new int[2];
 		Random rand = new Random();
-		// LegalMoves(state);
 
 		LegalMoves check = new LegalMoves(state);
 
@@ -28,33 +32,68 @@ public class AI {
 		while (true) {
 			r = rand.nextInt(8);
 			c = rand.nextInt(8);
-//		
-//		
 //		for (int r = 1; r < 8; r++) {
 //			for (int c = 1; c < 8; c++) {
 
-				if (check.isLegal(state.getBoard(), r, c)) {
-					move[0] = r;
-					move[1] = c;
-					return move;
-				}
-				System.out.println(r + " " + c);
+			if (check.isLegal(state.getBoard(), r, c)) {
+				move[0] = r;
+				move[1] = c;
+				return move;
+			}
+			System.out.println(r + " " + c);
 
-		
 		}
+	}
 
-//		int moveX = 4 ,moveY = 2;
-//		move[0] = moveX;
-//		move[1] = moveY;
+	private int getOpponent() {
+		if (player == 1)
+			return 2;
+		else
+			return 1;
+	}
 
-//		JsonObject json = new JsonObject();
-//		json.add(gson.toJson(move), json );
-//		JsonArray jarray = new JsonArray();
-//		jarray.add(json);
+	int minimax(int node, int depth, int isMaximizingPlayer, int alpha, int beta) {
+	    
+		// TODO If node is leaf, return value of node.
+		
+		
+//	    if (isMaximizingPlayer == player) {
+//	        int bestVal = -1000000; 
+//	    
+//	        for (node : node) {
+//	        	int value = minimax(node, depth+1, 2, alpha, beta);
+//	            bestVal = max( bestVal, value); 
+//	            alpha = max( alpha, bestVal);
+//	            if (beta <= alpha) 
+//	                break;
+//	        return bestVal;}
+//	    }
+//	    else {
+//
+//	    	int bestVal = +1000000;
+//	
+//	    	for (node : node) {
+//	            int value = minimax(node, depth+1, 1, alpha, beta);
+//	            bestVal = min(bestVal, value);
+//	            beta = min( beta, bestVal);
+//	            if (beta <= alpha)
+//	                break;
+//	            return bestVal;
+//	    	}
+//	    }
+		return 0;
+	}
 
-		// System.out.println("AI returning canned move for game state - " + state);
+	
+	private int min(int bestVal, int value) {
+		if (bestVal < value) 
+			return bestVal;
+		return value;
+	}
 
-		// return jarray;
-		// return move;
+	private int max(int bestVal, int value) {
+		if (bestVal > value) 
+			return bestVal;
+		return value;
 	}
 }
